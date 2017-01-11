6e96503062cba3b923412757199363beadd5df13
Cameron Tacklind <cameron@tacklind.com>
2017-01-10 23:21:26 -0800

    WIP: move to async functions

--- thermostat.js
+++ thermostat.js
@@ -21,6 +21,37 @@ function setHeaterOff() {
   setHeater(false);
 }
 
+var listSensors = denodeify(sensor.list.bind(sensor));
+
+async function getTemperatures() {
+
+  return new Promise((sensor.list((err, idList) => {
+    if (err) {
+      return callback(err);
+    }
+    that.get(idList, (err, result) => {
+    });
+  });
+
+
+  sensor.getAll((err, tempObj) => {
+    var sum = 0;
+    var len = 0;
+
+    for (var obj in tempObj) {
+      console.log(tempObj[obj]);
+      sum += tempObj[obj];
+      len++;
+    }
+
+    var average = sum / len;
+
+    console.log(average);
+
+    setHeater(average < threshold);
+  });
+}
+
 sensor.isDriverLoaded((err, isLoaded) => {
   console.log(err);
   if (!isLoaded) {
@@ -32,21 +63,6 @@ sensor.isDriverLoaded((err, isLoaded) => {
     console.log(listOfDeviceIds);
   });
   setInterval(() => {
-    sensor.getAll((err, tempObj) => {
-      var sum = 0;
-      var len = 0;
-
-      for (var obj in tempObj) {
-        console.log(tempObj[obj]);
-        sum += tempObj[obj];
-        len++;
-      }
-
-      var average = sum / len;
-
-      console.log(average);
-
-      setHeater(average < threshold);
-    });
+    ;
   }, interval);
 });
