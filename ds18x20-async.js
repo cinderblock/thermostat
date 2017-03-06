156c9276129fa9b4831e1a78d90a4c65a4f531c1
Cameron Tacklind <cameron@tacklind.com>
2017-03-06 12:54:00 -0800

    Add description of function

--- ds18x20-async.js
+++ ds18x20-async.js
@@ -6,6 +6,14 @@ module.exports = {
   get: denodeify(sensor.get.bind(sensor)),
   getAll: denodeify(sensor.getAll.bind(sensor)),
   isDriverLoaded: denodeify(sensor.isDriverLoaded.bind(sensor)),
+
+  /**
+   * Returns an object: {ids, temps, reduced, average}
+   * ids: array of temperature sensor IDs scanned
+   * temps: readings from temperature sensors
+   * reduced: object containing {sum, count} of valid temperature readings
+   * average: average temperature of valid temperature values
+   */
   getTemperatures: async function() {
     const ids = await this.list();
     const temps = await this.get(ids);
